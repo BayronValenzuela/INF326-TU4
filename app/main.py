5c69b66955fbdb3969aeb2b26309a3e407196e7a
VicenteRuizA <vicente.sd.ruiz@gmail.com>
2024-10-22 12:20:19 -0300

    eliminar comentarios

--- app/main.py
+++ app/main.py
@@ -23,12 +23,6 @@ app.add_middleware(
 mongodb_client = MongoClient("user_service_mongodb", 27017)
 user_service_db = mongodb_client.user_service
 
-"""
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(name)s:%(message)s"
-)
-"""
-
 app.include_router(professors.router, prefix="/api/v1/professors", tags=["Professors"])
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authenticate"])
 app.include_router(students.router, prefix="/api/v1/students", tags=["Students"])
