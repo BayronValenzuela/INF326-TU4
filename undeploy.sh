f3111c5cd855b15207789fc91125ba048b756744
mxidev <mazipantuflin@gmail.com>
2024-11-10 15:50:12 -0300

    Add scripts

---
+++ undeploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KUBECONFIG=/mnt/c/Users/mazip/Downloads/k8s-inf326-kubeconfig.yaml
+
+# Eliminar los recursos en orden inverso
+kubectl delete -f manifests/ingress.yaml --kubeconfig "$KUBECONFIG"
+kubectl delete -f manifests/hpa.yaml --kubeconfig "$KUBECONFIG"
+kubectl delete -f manifests/deployment.yaml --kubeconfig "$KUBECONFIG"
+kubectl delete -f manifests/rabbitmq.yaml --kubeconfig "$KUBECONFIG"
+kubectl delete -f manifests/statefulset.yaml --kubeconfig "$KUBECONFIG"
+
+# Verificar si hubo errores
+if [[ $? -ne 0 ]]; then
+  echo "Error deleting manifests. Some resources may not have been removed. Exiting."
+  exit 1
+fi
+
+echo "Manifests deleted successfully."
