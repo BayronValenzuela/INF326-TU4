e0e2be7b0f63125ed13fafc06cd69a6d3a598f6a
mxidev <mazipantuflin@gmail.com>
2024-11-10 14:55:34 -0300

    Improve structure to manifest

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,11 @@
 
 KUBECONFIG=/mnt/c/Users/mazip/Downloads/k8s-inf326-kubeconfig.yaml
 
-kubectl apply -f manifests/deployment.yaml --kubeconfig "$KUBECONFIG"
-kubectl apply -f manifests/service.yaml --kubeconfig "$KUBECONFIG"
-kubectl apply -f manifests/ingress.yaml --kubeconfig "$KUBECONFIG"
-kubectl apply -f manifests/secret.yaml --kubeconfig "$KUBECONFIG"
 kubectl apply -f manifests/statefulset.yaml --kubeconfig "$KUBECONFIG"
-kubectl apply -f manifests/volume.yaml --kubeconfig "$KUBECONFIG"
 kubectl apply -f manifests/rabbitmq.yaml --kubeconfig "$KUBECONFIG"
+kubectl apply -f manifests/deployment.yaml --kubeconfig "$KUBECONFIG"
+kubectl apply -f manifests/hpa.yaml --kubeconfig "$KUBECONFIG"
+kubectl apply -f manifests/ingress.yaml --kubeconfig "$KUBECONFIG"
 
 
 if [[ $? -ne 0 ]]; then
